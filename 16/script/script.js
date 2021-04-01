d045f7ccc31b13f5fd1c7a31720e7821d590e14d
eui <kje_0727@naver.com>
2021-04-02 01:04:26 +0900

    210401 기출유형문제4 - 가로형/페이드

---
+++ 16/script/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    // 상단 메뉴
+    $('.navi>li').mouseover(function(){
+        $(this).find('.submenu').stop().slideDown(400);
+    }).mouseout(function(){
+        $(this).find('.submenu').stop().slideUp(400);
+    });
+    
+    // 이미지 슬라이드(페이드)
+    $('.imgslide a:gt(0)').hide();
+    setInterval(function(){
+        $('.imgslide a:first-child').fadeOut()
+        .next('a').fadeIn()
+        .end().appendTo('.imgslide');
+    }, 3000);
+    
+    // 탭 메뉴
+    $('.tabmenu>li>a').click(function(){
+        $(this).parent().addClass("active").siblings().removeClass("active");
+        return false;
+    });
+    
+    // 레이어 팝업창
+    $('.notice li:first').click(function(){
+        $('#modal').addClass("active");
+    });
+    $('.btn').click(function(){
+        $('#modal').removeClass("active");
+    });
+});
